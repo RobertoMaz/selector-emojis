a4e1dc1f4a9afc53bcf965d2b0514c02730393c5
Roberto Mazzarello <rjmazzarello@gmail.com>
2022-09-20 13:12:27 -0300

    agregue un titulo

--- src/components/EmojiPickerInput.js
+++ src/components/EmojiPickerInput.js
@@ -8,10 +8,13 @@ function EmojiPickerInput() {
     const refInput = useRef(null);
 
     return ( 
+    <div>
+            <h1 className={styles.title}>Selector de Emojis</h1>
         <div className={styles.inputContainer}>
             <input ref={refInput}/>
             <EmojiPicker  ref={refInput}/>
         </div>
+    </div>       
     );
 }
 
